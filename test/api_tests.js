e940666200720fc743f5e5b64790ab80427b97b9
Paul Laskowski <pelaskowski@gmail.com>
2014-11-09 12:28:06 -0800

    setting up mocha tests

---
+++ test/api_tests.js
@@ -0,0 +1,18 @@
+var chai = require('chai');
+var chaihttp = require('chai-http');
+chai.use(chaihttp);
+
+require('../lib/server.js');
+
+var expect = chai.expect;
+
+describe('api create and read verification', function() {
+  
+  it('should be able to create a record with json content', function() {
+    
+  });
+
+  it('should be able to get json content from a record', function() {
+    
+  });
+});
